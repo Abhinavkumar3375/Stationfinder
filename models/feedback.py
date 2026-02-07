484fbc8a76e68cd96e33698ad0620bd17e9bc341
Abhinav Kumar <abhinavkumar3375@gmail.com>
2026-02-07 23:04:31 +0530

    Update feedback.py

--- models/feedback.py
+++ models/feedback.py
@@ -1,4 +1,4 @@
-# feedback.py - Optional class to define Feedback structure
+# feedback.py - Optional class to define Feedback structurerhrdzrxTSFgsRrfew    SDFDBRSEARWEQADFGFRHDGESFR
 class Feedback:
     def __init__(self, user, message, timestamp):
         self.user = user
